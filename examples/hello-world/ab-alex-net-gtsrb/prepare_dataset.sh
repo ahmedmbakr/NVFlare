94d3fd037ce16f059e516e929efc06ac9c21dd56
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2024-02-22 15:00:54 +0200

    - Fix prepare_dataset.sh file and make sure that it works for train and test images

--- examples/hello-world/ab-alex-net-gtsrb/prepare_dataset.sh
+++ examples/hello-world/ab-alex-net-gtsrb/prepare_dataset.sh
@@ -1,3 +1,4 @@
 DATASET_ROOT="~/data"
 
-python3 -c "import torchvision.datasets as datasets; datasets.GTSRB(root='${DATASET_ROOT}', transform=transforms, download=True, split="train")"
+python3 -c "import torchvision.datasets as datasets; datasets.GTSRB(root='${DATASET_ROOT}', download=True, split='train')"
+python3 -c "import torchvision.datasets as datasets; datasets.GTSRB(root='${DATASET_ROOT}', download=True, split='test')"
