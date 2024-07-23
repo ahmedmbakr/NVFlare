7e69f7bd83a43428554ff0440363e167129f7641
Ahmed Bakr <ambakr@crimson.ua.edu>
2024-07-23 10:28:58 -0500

    - Change the font size and add one global legend

--- examples/hello-world/parking-federated-training/utils/exprs_visualizer.py
+++ examples/hello-world/parking-federated-training/utils/exprs_visualizer.py
@@ -117,38 +117,34 @@ def visualize_clients_per_row(structure_dict):
                 max_loss = max(max_loss, max(data_dict[method][local_epochs_key_str][client_name]['train_loss']))
             y_limits_per_row.append(max_loss)
 
-    # fig.suptitle('Client 1 - FedAvg')
+    handles, labels = [], []
     for i, client_name in enumerate(['site-1', 'site-2', 'site-3', 'site-4']):
         for j, method in enumerate(structure_dict):
             for local_epochs_key_str in structure_dict[method].keys():
                 num_epochs = len(data_dict[method][local_epochs_key_str][client_name]['train_loss'])
                 epochs_arr = np.arange(num_epochs)
-                axs[i, j].plot(epochs_arr, data_dict[method][local_epochs_key_str][client_name]['train_loss'], label=f'{local_epochs_key_str}')
+                line, = axs[i, j].plot(epochs_arr, data_dict[method][local_epochs_key_str][client_name]['train_loss'])
+                if len(handles) < 2:
+                    handles.append(line)
+                    labels.append(f'{local_epochs_key_str}'.replace('-', ' '))
             if i == 0:
-                axs[i, j].set(title=f'{method}')
+                # Set the title of the first row and change the fontsize to 9
+                axs[i, j].set_title(f'{method}', fontsize='9')
             if j == 0:
-                axs[i, j].set(ylabel=f"Client {i+1} Loss")
+                axs[i, j].set_ylabel(f"Client {i+1} Loss", fontsize='9')
             else:
                 # Remove the y-axis numbers from all subplots except the first one
                 axs[i, j].yaxis.set_tick_params(labelleft=False)
 
             if i == 3: # assuming that we have only 4 clients
-                axs[i, j].set(xlabel="Epoch")
+                axs[i, j].set_xlabel("Epoch", fontsize='9')
            
             # Set y-axis limits
             axs[i, j].set_ylim([0, y_limits_per_row[i]]) 
 
-            # Display legends in the last column
-            if j == len(structure_dict) - 1: 
-                axs[i, j].legend()
-
             axs[i, j].grid()
-
-    # fig, axs = plt.subplots(4, 3)
-    # ax.plot(epochs_arr, data_dict[method]['1-local-epoch'][client_name]['train_loss'], label='1 local epoch')
-    # ax.plot(epochs_arr, data_dict[method]['2-local-epochs'][client_name]['train_loss'], label='2 local epochs')
-    # ax.set(xlabel="Epoch", ylabel="Loss", title="Client 1 - FedAvg")
-    # ax.grid()
+    # Create a single legend for all subplots and change the font size
+    fig.legend(handles, labels, loc='upper left', fontsize='9')
     plt.show()
 
     # Save the plot to a PDF file
