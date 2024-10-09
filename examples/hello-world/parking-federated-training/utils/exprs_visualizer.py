672bf0dd6b799642929663bc374a9cfbd92d66a7
Ahmed Bakr <ambakr@crimson.ua.edu>
2024-10-09 11:00:32 -0500

    - Update experiments viewer to include both experiments for Resnet and Sddnet

--- examples/hello-world/parking-federated-training/utils/exprs_visualizer.py
+++ examples/hello-world/parking-federated-training/utils/exprs_visualizer.py
@@ -20,57 +20,66 @@ def visualize_method_per_row(structure_dict):
         os.makedirs(output_dir)
     import pickle
     data_dict = {}
-    for method in structure_dict:
-        data_dict[method] = {}
-        for local_epochs_key_str in structure_dict[method].keys():
-            data_dict[method][local_epochs_key_str] = {}
-            for client_name in ['site-1', 'site-2', 'site-3', 'site-4']:
-                one_epoch_pkl_path = os.path.join(EXPERIMENTS_ROOT_DIR, structure_dict[method][local_epochs_key_str], client_name, 'overall_trackers.pkl')
-                with open(one_epoch_pkl_path, "rb") as f:
-                    data_dict[method][local_epochs_key_str][client_name] = pickle.load(f)
+    for network_name in structure_dict:
+        data_dict[network_name] = {}
+        network_structure_dict = structure_dict[network_name]
+        for method in network_structure_dict:
+            data_dict[network_name][method] = {}
+            for local_epochs_key_str in network_structure_dict[method].keys():
+                data_dict[network_name][method][local_epochs_key_str] = {}
+                for client_name in ['site-1', 'site-2', 'site-3', 'site-4']:
+                    one_epoch_pkl_path = os.path.join(EXPERIMENTS_ROOT_DIR, network_structure_dict[method][local_epochs_key_str], client_name, 'overall_trackers.pkl')
+                    with open(one_epoch_pkl_path, "rb") as f:
+                        data_dict[network_name][method][local_epochs_key_str][client_name] = pickle.load(f)
     print("Finished reading the pickle files.")
     method = 'FedAvg'
     client_name = 'site-1'
     # Visualize_method_per_row the losses
     #Create a figure and 4x3 subplots
-    fig, axs = plt.subplots(3, 4)
+    fig, axs = plt.subplots(4, 4)
     # Set the size of the figure to the width of a letter size paper
-    fig.set_size_inches(12, 8.5)
+    fig.set_size_inches(12.1, 8.5)
 
     y_limits_per_row = []
-    for i, method in enumerate(structure_dict):
-        for j, client_name in enumerate(['site-1', 'site-2', 'site-3', 'site-4']):
-            max_loss = 0
-            for local_epochs_key_str in structure_dict[method].keys():
-                max_loss = max(max_loss, max(data_dict[method][local_epochs_key_str][client_name]['train_loss']))
-            y_limits_per_row.append(max_loss)
+    for k, network_name in enumerate(structure_dict):
+        network_structure_dict = structure_dict[network_name]
+        for i, method in enumerate(network_structure_dict):
+            for j, client_name in enumerate(['site-1', 'site-2', 'site-3', 'site-4']):
+                max_loss = 0
+                for local_epochs_key_str in network_structure_dict[method].keys():
+                    max_loss = max(max_loss, max(data_dict[network_name][method][local_epochs_key_str][client_name]['train_loss']))
+                y_limits_per_row.append(max_loss)
 
     # fig.suptitle('Client 1 - FedAvg')
-    for i, method in enumerate(structure_dict):
-        for j, client_name in enumerate(['site-1', 'site-2', 'site-3', 'site-4']):
-            for local_epochs_key_str in structure_dict[method].keys():
-                num_epochs = len(data_dict[method][local_epochs_key_str][client_name]['train_loss'])
-                epochs_arr = np.arange(num_epochs)
-                axs[i, j].plot(epochs_arr, data_dict[method][local_epochs_key_str][client_name]['train_loss'], label=f'{local_epochs_key_str}')
-            if i == 0:
-                axs[i, j].set(title=f'Client {j+1}')
-            if j == 0:
-                axs[i, j].set(ylabel=f"{method} Loss")
-            else:
-                # Remove the y-axis numbers from all subplots except the first one
-                axs[i, j].yaxis.set_tick_params(labelleft=False)
+    i = 0
+    for network_name in structure_dict:
+        network_structure_dict = structure_dict[network_name]
+        for method in network_structure_dict:
+            for j, client_name in enumerate(['site-1', 'site-2', 'site-3', 'site-4']):
+                for local_epochs_key_str in network_structure_dict[method].keys():
+                    num_epochs = len(data_dict[network_name][method][local_epochs_key_str][client_name]['train_loss'])
+                    epochs_arr = np.arange(num_epochs)
+                    axs[i, j].plot(epochs_arr, data_dict[network_name][method][local_epochs_key_str][client_name]['train_loss'], label=f'{local_epochs_key_str}')
+                if i == 0:
+                    axs[i, j].set(title=f'Client {j+1}')
+                if j == 0:
+                    axs[i, j].set(ylabel=f"{method} Loss\nfor {network_name}")
+                else:
+                    # Remove the y-axis numbers from all subplots except the first one
+                    axs[i, j].yaxis.set_tick_params(labelleft=False)
 
-            if i == len(structure_dict) - 1:
-                axs[i, j].set(xlabel="Epoch")
-           
-            # Set y-axis limits
-            axs[i, j].set_ylim([0, y_limits_per_row[i]]) 
+                if i == len(network_structure_dict) - 1:
+                    axs[i, j].set(xlabel="Epoch")
+            
+                # Set y-axis limits
+                axs[i, j].set_ylim([0, y_limits_per_row[i]]) 
 
-            # Display legends in the last column
-            if j == 3: # assuming that we have only 4 clients
-                axs[i, j].legend()
+                # Display legends in the last column
+                if j == 3: # assuming that we have only 4 clients
+                    axs[i, j].legend()
 
-            axs[i, j].grid()
+                axs[i, j].grid()
+            i += 1
 
     # fig, axs = plt.subplots(4, 3)
     # ax.plot(epochs_arr, data_dict[method]['1-local-epoch'][client_name]['train_loss'], label='1 local epoch')
@@ -85,47 +94,54 @@ def visualize_method_per_row(structure_dict):
     print(f"Losses plot saved to {losses_img_path}")
 
     # Draw mAP vs Rounds
-    fig, axs = plt.subplots(3, 4)
+    fig, axs = plt.subplots(4, 4)
     # Set the size of the figure to the width of a letter size paper
-    fig.set_size_inches(12, 8.5)
+    fig.set_size_inches(12.1, 8.5)
 
     y_limits_per_row = []
-    for i, method in enumerate(structure_dict):
-        for j, client_name in enumerate(['site-1', 'site-2', 'site-3', 'site-4']):
-            max_loss = 0
-            for local_epochs_key_str in structure_dict[method].keys():
-                max_loss = max(max_loss, max(data_dict[method][local_epochs_key_str][client_name]['train_loss']))
-            y_limits_per_row.append(max_loss)
+    for network_name in structure_dict:
+        network_structure_dict = structure_dict[network_name]
+        for i, method in enumerate(network_structure_dict):
+            for j, client_name in enumerate(['site-1', 'site-2', 'site-3', 'site-4']):
+                max_loss = 0
+                for local_epochs_key_str in network_structure_dict[method].keys():
+                    max_loss = max(max_loss, max(data_dict[network_name][method][local_epochs_key_str][client_name]['train_loss']))
+                y_limits_per_row.append(max_loss)
 
     # fig.suptitle('Client 1 - FedAvg')
-    for i, method in enumerate(structure_dict):
-        for j, client_name in enumerate(['site-1', 'site-2', 'site-3', 'site-4']):
-            for local_epochs_key_str in structure_dict[method].keys():
-                mAP = [x['mAP'] for x in data_dict[method][local_epochs_key_str][client_name]['val_acc']]
-                # If the method is FedAvg when we have 2 local epochs, take only the even rounds. This is a hack because this was the only experiment were I was evaluating every 1 local epochs instead of every 2 local epochs.
-                if method == 'FedAvg' and local_epochs_key_str == '2-local-epochs':
-                    mAP = mAP[1::2]
-                rounds_arr = np.arange(len(mAP))
-                axs[i, j].plot(rounds_arr, mAP, label=f'{local_epochs_key_str}')
-            if i == 0:
-                axs[i, j].set(title=f'Client {j+1}')
-            if j == 0:
-                axs[i, j].set(ylabel=f"{method} mAP")
-            else:
-                # Remove the y-axis numbers from all subplots except the first one
-                axs[i, j].yaxis.set_tick_params(labelleft=False)
+    i = 0
+    for network_name in structure_dict:
+        network_structure_dict = structure_dict[network_name]
+        for method in network_structure_dict:
+            for j, client_name in enumerate(['site-1', 'site-2', 'site-3', 'site-4']):
+                for local_epochs_key_str in network_structure_dict[method].keys():
+                    mAP = [x['mAP'] for x in data_dict[network_name][method][local_epochs_key_str][client_name]['val_acc']]
+                    # If the method is FedAvg when we have 2 local epochs, take only the even rounds. This is a hack because this was the only experiment were I was evaluating every 1 local epochs instead of every 2 local epochs.
+                    if method == 'FedAvg' and local_epochs_key_str == '2-local-epochs':
+                        mAP = mAP[1::2]
+                    rounds_arr = np.arange(len(mAP))
+                    axs[i, j].plot(rounds_arr, mAP, label=f'{local_epochs_key_str}')
+                if i == 0:
+                    axs[i, j].set(title=f'Client {j+1}')
+                if j == 0:
+                    axs[i, j].set(ylabel=f"{method} mAP\nfor {network_name}")
+                else:
+                    # Remove the y-axis numbers from all subplots except the first one
+                    axs[i, j].yaxis.set_tick_params(labelleft=False)
 
-            if i == len(structure_dict) - 1:
-                axs[i, j].set(xlabel="Round")
-           
-            # Set y-axis limits
-            axs[i, j].set_ylim([0, 1.1]) 
+                if i == len(network_structure_dict) - 1:
+                    axs[i, j].set(xlabel="Round")
+            
+                # Set y-axis limits
+                axs[i, j].set_ylim([0, 1.1]) 
 
-            # Display legends in the last column
-            if j == 3: # assuming that we have only 4 clients
-                axs[i, j].legend(loc='lower right')
+                # Display legends in the last column
+                if j == 3: # assuming that we have only 4 clients
+                    axs[i, j].legend(loc='lower right')
 
-            axs[i, j].grid()
+                axs[i, j].grid()
+
+            i += 1
 
     # fig, axs = plt.subplots(4, 3)
     # ax.plot(epochs_arr, data_dict[method]['1-local-epoch'][client_name]['train_loss'], label='1 local epoch')
@@ -254,17 +270,25 @@ def visualize_clients_per_row(structure_dict):
 
 EXPERIMENTS_ROOT_DIR = "/home/bakr/NVFlare/examples/hello-world/parking-federated-training/exprs"
 structure_dict = {
-    'FedAvg': {
-        '1-local-epoch': 'expr-10',
-        '2-local-epochs': 'expr-09'
+    'Resnet50': {
+        'FedAvg': {
+            '1-local-epoch': 'expr-10',
+            '2-local-epochs': 'expr-09'
+        },
+        'FedProx': {
+            '1-local-epoch': 'expr-11',
+            '2-local-epochs': 'expr-12'
+        },
+        'SCAFFOLD': {
+            '1-local-epoch': 'expr-13',
+            '2-local-epochs': 'expr-14'
+        }
     },
-    'FedProx': {
-        '1-local-epoch': 'expr-11',
-        '2-local-epochs': 'expr-12'
-    },
-    'SCAFFOLD': {
-        '1-local-epoch': 'expr-13',
-        '2-local-epochs': 'expr-14'
+    'Sddnet': {
+        'SCAFFOLD': {
+        '1-local-epoch': 'expr-15',
+        '2-local-epochs': 'expr-15' # TODO: AB: Unitl I finish experiment 16, I use experiment 15 for both 1 and 2 local epochs
+        }
     }
 }
 
