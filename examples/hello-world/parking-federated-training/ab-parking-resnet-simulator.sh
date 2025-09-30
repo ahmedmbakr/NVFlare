79228cd469819ddee1d3e524e2a67edefc401398
Ahmed Bakr <ambakr@crimson.ua.edu>
2025-09-29 19:33:55 -0500

    - Add a new aggregator for trimmed mean. This aggregator uses scatter and gather method.

--- examples/hello-world/parking-federated-training/ab-parking-resnet-simulator.sh
+++ examples/hello-world/parking-federated-training/ab-parking-resnet-simulator.sh
@@ -3,7 +3,9 @@ cd ~/NVFlare
 source ~/anaconda3/bin/activate nvflare
 rm -rf simulator-example
 mkdir -p simulator-example/workspace
+echo "Created simulator-example directory."
 cp -r examples/hello-world/parking-federated-training/ simulator-example/
+echo "Copying of parking-federated-training directory completed."
 mkdir -p simulator-example/workspace/
 ln -sf ~/CNR-EXT/ simulator-example/workspace/data
 nvflare simulator -w simulator-example/workspace/ -n 1 -t 1 simulator-example/parking-federated-training/jobs/parking-federated-training/
